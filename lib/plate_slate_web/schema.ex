cf1ea0b8026f0d528e88f11eb9988a84aa34c4f9
johninvictus <invictusnitude@gmail.com>
2019-08-12 02:25:26 +0300

    added supscription for new order

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -4,6 +4,7 @@ defmodule PlateSlateWeb.Schema do
   alias PlateSlateWeb.Resolvers
 
   import_types(PlateSlateWeb.Schema.MenuTypes)
+  import_types(PlateSlateWeb.Schema.OrderingTypes)
   import_types(PlateSlateWeb.Schema.CustomTypes)
 
   query do
@@ -14,16 +15,30 @@ defmodule PlateSlateWeb.Schema do
     end
 
     field :search, list_of(:search_result) do
-      arg :matching, non_null(:string)
+      arg(:matching, non_null(:string))
 
-      resolve &Resolvers.Menu.search/3
+      resolve(&Resolvers.Menu.search/3)
     end
   end
 
   mutation do
     field :create_menu_item, :menu_item_result do
-      arg :input, non_null(:menu_item_input)
-      resolve &Resolvers.Menu.create_item/3
+      arg(:input, non_null(:menu_item_input))
+      resolve(&Resolvers.Menu.create_item/3)
+    end
+
+    field :place_order, :order_result do
+      arg(:input, non_null(:place_order_input))
+      resolve(&Resolvers.Ordering.place_order/3)
+    end
+  end
+
+  subscription do
+    field :new_order, :order do
+
+      config fn _arg, _info ->
+        {:ok, topic: "*"}
+      end
     end
   end
 
